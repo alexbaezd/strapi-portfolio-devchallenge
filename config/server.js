5532c09d9b5c390fc9a46dc3a43240a550572e08
Alejandro Báez <baezdalex@gmail.com>
2021-08-13 13:23:47 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '93cf7261029ef894a5fec9fb8ab1f4ab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13ddd053-3351-4f97-902c-3eb41722b303'
+};
